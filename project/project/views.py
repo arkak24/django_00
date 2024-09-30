1ec6e90a9494b32197d92a36e3f012eb01baff48
arkak24 <arkak.brj@gmail.com>
2024-10-01 03:25:51 +0530

    added html and css(templates and static)

--- project/project/views.py
+++ project/project/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def  home(request):
-    return HttpResponse("Hello world, you are at chai aur django homepage")
+    # return HttpResponse("Hello world, you are at chai aur django homepage")
+    return render(request, 'index.html')
+    # we can make any other folder inside the templates folder, let it be 'website'
+    # and inside it be the index.html file
+    # for that we have to write the path as : return render(request, 'website/index.html')
 
 def  about(request):
     return HttpResponse("Hello world, you are at chai aur django about page")
